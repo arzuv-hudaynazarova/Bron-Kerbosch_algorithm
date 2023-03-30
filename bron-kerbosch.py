e907122172d1225c400e2a695b46bfbde3ff9887
Arzuv Hudaynazarova <126810475+arzuv-hudaynazarova@users.noreply.github.com>
2023-03-30 22:35:29 +0300

    Update bron-kerbosch.py

--- bron-kerbosch.py
+++ bron-kerbosch.py
@@ -6,18 +6,18 @@ Burada 'alice_in_wonderland.txt' dosyasındaki belirli kelimelerin kaç kez geç
 Her hangi bir arama algoritması kullanılmamıştır. Pythonda olan hazır fonksyon count() kullanılmıştır.
 """
 
-# "alice_in_wonderland.txt" dosyasını okuyarak içeriği bir dize değişkenine atarız
+# "alice_in_wonderland.txt" dosyasını okuyarak içeriği bir dize değişkenine atadım
 with open("alice_in_wonderland.txt", "r") as f:
     text = f.read()
 
-# Sayısını hesaplamak istediğimiz kelimeleri bir liste olarak belirleriz
+# Sayısını hesaplamak istediğim kelimeleri bir liste olarak belirledim
 aranan_kelimeler = ["upon", "sigh", "Dormouse", "jury-box", "swim"]
 
-# Her kelimenin sayısını saymak için bir döngü oluştururuz
+# Her kelimenin sayısını saymak için bir döngü oluşturdum
 for kelime in aranan_kelimeler:
-    # "count()" yöntemiyle kelimenin sayısını hesaplarız
+    # "count()" yöntemiyle kelimenin sayısını hesapladım
     sayi = text.count(kelime)
-    # Hesaplanan sayıyı ekrana yazdırırız
+    # Hesaplanan sayıyı ekrana yazdırdım
     print(f"'{kelime}' kelimesi 'alice_in_wonderland.txt' dosyasında {sayi} kez tekrar ediyor.")
 
     
